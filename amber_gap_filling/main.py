d7cec6cc1dff3cf83dbefd73c5f1c13c2a99b0d3
Bram Meir <159529490+BramMeir@users.noreply.github.com>
2025-10-31 20:43:21 +0100

    Add MAE print on comparison with amber's method

--- amber_gap_filling/main.py
+++ amber_gap_filling/main.py
@@ -20,8 +20,8 @@ def test_different_gf_techniques_amber():
 
     df_errors, df_stderr = Test_techniques_differentgaplengths(
         df_all,
-        name_fulldata='Betel',
-        name_model='Betel_ERA5',
+        name_fulldata='Ylijoki',
+        name_model='Ylijoki_ERA5',
         dictionarytechniques={
             "debmodelReg": [60, 1, 'both'],
             "debmodelMeanbias": [60, 1, 'both'],
@@ -53,19 +53,21 @@ def test_SARIMA_approach(seed=47):
     df_Turku = df_Turku.resample("h").mean().interpolate()
 
     # Select the target station time series
-    series_full = df_Turku["Betel"]
+    series_full = df_Turku["Ylijoki"]
 
     # Select the exogenous data (other stations in the Turku dataset)
-    exog_df_full = df_Turku.drop(columns=["Betel"])
+    exog_df_full = df_Turku.drop(columns=["Ylijoki"])
 
     # Keep track of the forecast errors
     mse_list = []
+    mae_list = []
 
     for hours_to_forecast in [5, 7, 12, 24, 48, 168, 336]:
         print(f"\nForecasting {hours_to_forecast} hours into the future:")
 
         # Repeat the forecasting multiple times to get an average error
         temp_mse_list = []
+        temp_mae_list = []
         for _ in range(100):
             # Select random 2 weeks + forecast horizon for testing from series and exog_df
             max_start = series_full.index.max() - pd.DateOffset(hours=hours_to_forecast + 24 * 14)
@@ -87,15 +89,23 @@ def test_SARIMA_approach(seed=47):
                 plot=False,
             )
             temp_mse_list.append(errors["MSE"])
+            temp_mae_list.append(errors["MAE"])
 
         average_mse = sum(temp_mse_list) / len(temp_mse_list)
         mse_list.append(average_mse)
+
+        average_mae = sum(temp_mae_list) / len(temp_mae_list)
+        mae_list.append(average_mae)
 
     print("\nAverage MSE for different forecast horizons:")
     for hours, mse in zip([5, 7, 12, 24, 48, 168, 336], mse_list):
         print(f"{hours} hours: MSE = {mse}")
 
+    print("\nAverage MAE for different forecast horizons:")
+    for hours, mae in zip([5, 7, 12, 24, 48, 168, 336], mae_list):
+        print(f"{hours} hours: MAE = {mae}")
+
 
 if __name__ == "__main__":
-    # test_different_gf_techniques_amber()
-    test_SARIMA_approach()
+    test_different_gf_techniques_amber()
+    # test_SARIMA_approach()
