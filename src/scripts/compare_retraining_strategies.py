2ed7f13b82e3ce268cb68b5b7758e4e0d789743b
Bram Meir <159529490+BramMeir@users.noreply.github.com>
2026-05-09 15:53:13 +0200

    Improve comparison between retraining strategies script

--- src/scripts/compare_retraining_strategies.py
+++ src/scripts/compare_retraining_strategies.py
@@ -11,7 +11,7 @@ import pandas as pd
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from src.utils.select_LASSO_stations import select_LASSO_stations
 from src.models.arima.train import train_sarima_model
-from src.models.arima.repeat_forecast import _run_single_forecast as run_arimax
+from src.evaluation.evaluate_forecasts import evaluate_forecasts
 from src.utils.generate_forecast_start import generate_forecast_start
 
 
@@ -19,39 +19,42 @@ from src.utils.generate_forecast_start import generate_forecast_start
 DATASETS = {
     "KMI": {
         "file": "data/Full_AWS/preprocessed_10m_2022_2025.csv",
-        "stations": ["MELLE"]
+        "stations": ["MELLE", "DIEPENBEEK", "HUMAIN", "SINT-KATELIJNE-WAVER", "ERNAGE"]
     },
     "TURKU": {
         "file": "data/Turku/Turku_preprocessed.csv",
-        "stations": ["Betel", "Virastotalo", "Ylijoki"]
+        "stations": ["Betel", "Virastotalo", "Ylijoki", "Kurala"]
     },
     "SYNTHETIC": {
         "file": "data/Synthetic/temperature_data.csv",
         "stations": [
             "Stadhuis_Brussel_Grote_Markt",
-            # "Tielt",
-            # "Etalle",
-            # "Markt_Hoei"
+            "Tielt",
+            "Etalle",
+            "Markt_Hoei",
+            "Stadhuis_Antwerpen_Grote_Markt",
+            "Gembloux",
+            "Slag_om_Ardennen_Museum_La_Roche_en_Ardenne",
+            "Grote_Markt_Kortrijk",
         ]
     }
 }
 
-MODEL_CONFIGS = {
+MODEL_CONFIGS = [
     ("no_retrain", None),
 
+    ("full_3d", 3),
+    ("full_15d", 15),
+
     ("inc_3d", 3),
-    ("inc_9d", 9),
     ("inc_15d", 15),
 
-    ("full_3d", 3),
     ("full_9d", 9),
-    ("full_15d", 15)
-}
+    ("inc_9d", 9),
+]
 
 # Seed that is used to define the training periods (for reproducibility)
 SEED = 47
-
-NUMBER_OF_REPEATS = 1
 
 
 def run_single_experiment(task):
@@ -79,7 +82,7 @@ def run_single_experiment(task):
         seed=SEED,
         repeat_id=repeat_id,
         max_history_days=8 * 7,        # Max of 8 weeks of history to train the model on
-        max_horizon=90 * 24 + 24       # Max 90 days of simulation + 1 day of forecast to evaluate on
+        max_horizon=60 * 24 + 24       # Max 60 days of simulation + 1 day of forecast to evaluate on
     )
 
     # Determine the required training period for the model
@@ -94,14 +97,14 @@ def run_single_experiment(task):
         exog_df = exog_df[selected_stations]
 
     # Evaluate all the different retraining strategies for the given training period and forecast start
-    results = []
+    output_results = []
     for model_type, retrain_gap in MODEL_CONFIGS:
 
         # Initial training of the model on the training period
         train_start = forecast_start - pd.Timedelta(weeks=training_weeks)
         train_end = forecast_start
 
-        model = train_sarima_model(
+        results = train_sarima_model(
             series=series[train_start:train_end],
             exog_df=exog_df[train_start:train_end],
             arima_order=(2, 0, 0),
@@ -110,98 +113,93 @@ def run_single_experiment(task):
         )
 
         last_retrain_time = forecast_start
+        last_update_time = forecast_start
 
         current_time = forecast_start
-        end_time = forecast_start + pd.Timedelta(days=90)  # Simulate for 90 days
+        end_time = forecast_start + pd.Timedelta(days=60)  # Simulate for 60 days
 
         # Rolling simulation
         while current_time < end_time:
-
-            # Forecast the next 24 hours
-            forecast_end = current_time + pd.Timedelta(days=1)
-
-            result = run_arimax(
-                repeat_id,
-                series=series,
-                exog_df=exog_df,
-                model=model,
-                start_date=train_start,
-                end_date=forecast_end,
-                hours_to_forecast=24,
-                arima_order=(2, 0, 0),
-                seasonal_order=(1, 0, 1, 24),
-                confidence_score=False,
-                use_LASSO_selection=False,
-                max_iter=1000,
-                plot=False
-            )
-
-            mae, mse = result[:2]
-
             retrain_event = 0
 
             # Check for retraining
-            if model_type != "no_retrain":
-                if (current_time - last_retrain_time) >= pd.Timedelta(days=retrain_gap):
-                    retrain_event = 1
+            if model_type != "no_retrain" and (current_time - last_retrain_time) >= pd.Timedelta(days=retrain_gap):
+                retrain_event = 1
 
-                    # Start the timing of the retraining
-                    start_retraining_time = pd.Timestamp.now()
+                # Start the timing of the retraining
+                start_retraining_time = pd.Timestamp.now()
 
-                    # Determine what retraining strategy to use
-                    if model_type.startswith("full"):
-                        # Full retraining
-                        new_train_start = current_time - pd.Timedelta(weeks=training_weeks)
-                        new_train_end = current_time
+                # Determine what retraining strategy to use
+                if model_type.startswith("full"):
+                    # Full retraining
+                    new_train_start = current_time - pd.Timedelta(weeks=training_weeks)
+                    new_train_end = current_time
 
-                        model = train_sarima_model(
-                            series=series[new_train_start:new_train_end],
-                            exog_df=exog_df[new_train_start:new_train_end],
-                            arima_order=(2, 0, 0),
-                            seasonal_order=(1, 0, 1, 24),
-                            max_iter=1000
-                        )
+                    results = train_sarima_model(
+                        series=series[new_train_start:new_train_end],
+                        exog_df=exog_df[new_train_start:new_train_end],
+                        arima_order=(2, 0, 0),
+                        seasonal_order=(1, 0, 1, 24),
+                        max_iter=1000
+                    )
 
-                        train_start = new_train_start
+                    train_start = new_train_start
 
-                    elif model_type.startswith("inc"):
-                        # Incremental retraining
-                        update_start = last_retrain_time + pd.Timedelta(hours=1)
-                        update_end = current_time - pd.Timedelta(hours=1)
+                elif model_type.startswith("inc"):
+                    # Incremental retraining
+                    update_start = last_update_time + pd.Timedelta(hours=1)
+                    update_end = current_time
 
-                        # Make sure to print what the range of data was that is added (and what the model already had seen)
-                        print(f"Retraining with data from {update_start} to {update_end}")
-                        # Print what model already had seen
-                        print(f"Model already had seen data up to {last_retrain_time}")
+                    # Refit=True so model parameters can be updated based on the new data
+                    results = results.append(
+                        endog=series[update_start:update_end],
+                        exog=exog_df[update_start:update_end],
+                        refit=True
+                    )
 
-                        # Refit=True so model parameters can be updated based on the new data
-                        model = model.append(
-                            endog=series[update_start:update_end],
-                            exog=exog_df[update_start:update_end],
-                            refit=True
-                        )
+                end_retraining_time = pd.Timestamp.now()
+                retraining_duration = end_retraining_time - start_retraining_time
 
-                    end_retraining_time = pd.Timestamp.now()
-                    retraining_duration = end_retraining_time - start_retraining_time
+                last_retrain_time = current_time
 
-                    last_retrain_time = current_time - pd.Timedelta(hours=1)
+            # If the model was not retrained, still update the model state with the new data
+            if not retrain_event and current_time > last_update_time:
+                update_start = last_update_time + pd.Timedelta(hours=1)
+                update_end = current_time
 
-            results.append([
+                results = results.append(
+                    endog=series[update_start:update_end],
+                    exog=exog_df[update_start:update_end],
+                    refit=False
+                )
+
+            # Forecast the next 24 hours
+            forecast_end = current_time + pd.Timedelta(days=1)
+            pred = results.get_forecast(steps=24, exog=exog_df[current_time + pd.Timedelta(hours=1):forecast_end])
+            test_series = series[current_time + pd.Timedelta(hours=1):forecast_end]
+
+            # Create index for forecasted values (same as test index)
+            y_forecasted = pd.Series(pred.predicted_mean.values, index=test_series.index)
+            errors = evaluate_forecasts(test_series, y_forecasted)
+            print(errors)
+
+            output_results.append([
                 dataset_name,
                 station,
                 repeat_id,
                 model_type,
                 current_time,
-                mae,
-                mse,
+                errors["MAE"],
+                errors["MSE"],
                 retrain_event,
                 retraining_duration.total_seconds() if retrain_event else 0
             ])
 
             # Move to the next step
+            last_update_time = current_time
             current_time += pd.Timedelta(days=3)  # Step of 3 days between forecasts
 
-    return results
+    return output_results
 
 
 def run_all_experiments(repeat_id, training_weeks, dataset_name=None):
@@ -302,5 +300,5 @@ if __name__ == "__main__":
     os.makedirs("output/retraining_results", exist_ok=True)
 
     # Run all experiments and save results to CSV
-    nr_training_weeks = 2
+    nr_training_weeks = 8
     run_all_experiments(args.repeat_id, nr_training_weeks, dataset_name=args.dataset)
